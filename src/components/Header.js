626c95e9518610e0c9a7049314dd5fe43a26feec
Rodrigo Oliveira <rodrigo_oliveira@live.com>
2024-02-21 19:09:01 -0300

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import Logo from './Logo';
+
+function Header() {
+    return (
+        <header className='Header'>
+            <Logo></Logo>
+            
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import logo from '../images/logo.svg';
+import './Logo.css';
+
+function Logo() {
+    return (
+        <div className='logo'>
+            <img className='logo__img' src={logo} alt='Books'></img>
+            <p>Books</p>
+        </div>
+    );
+}
+
+export default Logo;
